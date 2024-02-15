091ee0c9f3d9c6e67e136ccc323e6aa71f532b47
vetak8 <72986139+vetak8@users.noreply.github.com>
2024-02-15 16:23:08 +0300

    Построил GradientBoosting

--- intern/20_grarient_boosting/.ipynb_checkpoints/solution-checkpoint.py
+++ intern/20_grarient_boosting/.ipynb_checkpoints/solution-checkpoint.py
@@ -1,22 +1,65 @@
+from typing import Callable, Union, Tuple
 import numpy as np
-import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 class GradientBoostingRegressor:
-    """Gradient boosting regressor."""
-    def __init__(self):
+    '''
+    Функция реализует градиентный бустинг
+    '''
+    def __init__(
+        self,
+        n_estimators=100,
+        learning_rate=0.1,
+        max_depth=3,
+        min_samples_split=2,
+        loss="mse",
+        verbose=False,
+    ):
+        self.trees_ = []
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.loss = loss
+        self.verbose = verbose
         self.base_pred_ = None
 
-    def fit(self, X, y):
-        """Fit the model to the data.
+    def _mse(self,y_true: np.ndarray, y_pred: np.ndarray) -> Tuple[float, np.ndarray]:
+        """Mean squared error loss function and gradient."""
+        error = y_pred - y_true
+        loss = np.mean(error ** 2)
+        grad = error
+        return loss, grad
 
-        Args:
-            X: array-like of shape (n_samples, n_features)
-            y: array-like of shape (n_samples,)
+    def _validate_loss_function(self, loss: Union[str, Callable]) -> Callable:
+        '''Validate loss function'''
+        if isinstance(loss, str):
+            if loss == 'mse':
+                return self._mse
+        elif callable(loss):
+            return loss
+        else:
+            raise ValueError('Loss must be a callable function')
 
-        Returns:
-            GradientBoostingRegressor: The fitted model.
-        """
+    def fit(self, X: np.ndarray, y: np.ndarray) -> 'GradientBoostingRegressor':
+        """Fit the model to the data."""
         self.base_pred_ = np.mean(y)
+        y_pred = np.full_like(y, self.base_pred_, dtype=np.float64)
+
+
+        loss_func = self._validate_loss_function(self.loss)
+
+        for _ in range(self.n_estimators):
+            loss, grad = loss_func(y, y_pred)
+            tree = DecisionTreeRegressor(max_depth=self.max_depth,
+                                          min_samples_split=self.min_samples_split)
+            tree.fit(X, -grad)
+            self.trees_.append(tree)
+            y_pred += self.learning_rate * tree.predict(X)
+            
+            if self.verbose:
+                print(f"Iteration: {_ + 1}, Loss: {loss}")
+        
         return self
 
     def predict(self, X):
@@ -28,10 +71,12 @@ class GradientBoostingRegressor:
         Returns:
             y: array-like of shape (n_samples,)
             The predict values.
-            
+
         """
-        # YOUR CODE HERE...
         predictions = np.full(X.shape[0], self.base_pred_)
+        for tree in self.trees_:
+            predictions += self.learning_rate * tree.predict(X)
         return predictions
 
-        
+if __name__ == '__main__':
+    
